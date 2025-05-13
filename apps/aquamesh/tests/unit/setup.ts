c2b41e83cb64b28a3e613f8375a4d86940700195
Cosme Valera <cosmevalerareales@gmail.com>
2025-05-13 23:23:28 +0200

    AquaMesh:MAIN Remove previous Unit tests

--- apps/aquamesh/tests/unit/setup.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom/vitest'
-import { expect } from 'vitest'
-import matchers from '@testing-library/jest-dom/matchers'
-
-// Add the custom matchers from jest-dom
-expect.extend(matchers) 

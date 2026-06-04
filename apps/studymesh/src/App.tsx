284614757d54ef60f892f22dae5b9fdd879b1abf
Pichikachu <pichikachu@users.noreply.github.com>
2026-06-04 16:23:45 +0200

    Add Web Clipper FAB to workspace UI

--- apps/studymesh/src/App.tsx
+++ apps/studymesh/src/App.tsx
@@ -18,6 +18,7 @@ import WorkspaceStudioShell from './components/workspace/WorkspaceStudioShell'
 import DashboardProvider from './components/Dasboard/DashboardProvider'
 import LayoutProvider from './components/Layout/LayoutProvider'
 import StudyMeshLanding from './components/landing/StudyMeshLanding'
+import WebClipperFAB from './WebClipperFAB'
 import { useWorkspaceActions } from './customHooks/useWorkspaceActions'
 import LocalAiDebugPanel from './components/debug/LocalAiDebugPanel'
 import { cancelAllLocalAiSessions } from './studyPack/ai'
@@ -134,6 +135,7 @@ const WorkspacePage = () => {
           <Dashboards />
         </WorkspaceStudioShell>
         <WorkspaceOnboarding />
+        <WebClipperFAB />
       </Main>
     </Box>
   )
